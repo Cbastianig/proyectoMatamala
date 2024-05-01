17d9846a4f4227fbee1f35059add0dd1ca23dc6c
sbadilla <sbadilla@idgterragis.cl>
2024-05-01 16:43:01 -0400

    2do comit

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,52 +13,13 @@ import Acceso_denegado from './(main)/accessDenied';
 
 export default async function RootLayout({ children }) {
 
-    const env = await getEnv(); 
+  
 
     const modo = (await getCookie('modo')) ?? 'light';
     const tema = (await getCookie('tema')) ?? 'blue';
 
     const path = await getPath();
-    const headers = new Headers();
-    const cookieValue = await getCookie('PHPSESSID');
-    if (!cookieValue && env !== 'LOCAL_DEV') return (<html lang="es" suppressHydrationWarning>
-        <head>
-            <link id="theme-link" href={`${path}/theme/theme-${modo}/${tema}/theme.css`} rel="stylesheet"></link>
-        </head>
-        <body>
-            <PrimeReactProvider>
-                <LayoutProvider modo={modo} tema={tema} path={path}><Acceso_denegado layout={false}></Acceso_denegado></LayoutProvider>
-            </PrimeReactProvider>
-        </body>
-    </html>)
-    headers.append('Cookie', cookieValue);
-    let requestDomain = 'https://desa-gis.gassur.cl';
-    if(env === 'DESA_GIS') requestDomain = 'https://desa-gis.gassur.cl'; 
-    if(env === 'PRODUCCION') requestDomain = 'https://gis.gassur.cl';
-    if(env!=="LOCAL_DEV"){
-    //ENVIAR LA COOCKIE COMO POST
-    const phpsession = await fetch(`${requestDomain}/app/Get_SessionInfo.php`, {
-        method: 'POST',
-        headers: headers,
-        credentials: 'include',
-        body: JSON.stringify({ cookie: cookieValue })
-    }).then(res => res.json()).catch(err => console.log(err));
-
-  
-        if (phpsession.respuesta.status !== 'ok') return (
-            <html lang="es" suppressHydrationWarning>
-                <head>
-                    <link id="theme-link" href={`${path}/theme/theme-${modo}/${tema}/theme.css`} rel="stylesheet"></link>
-                </head>
-                <body>
-                    <PrimeReactProvider>
-                        <LayoutProvider modo={modo} tema={tema} path={path}><Acceso_denegado layout={false}></Acceso_denegado></LayoutProvider>
-                    </PrimeReactProvider>
-                </body>
-            </html>
-        )
-    }
-  
+ 
 
     return (
         <html lang="es" suppressHydrationWarning>
